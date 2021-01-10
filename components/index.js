8aae6e2b7e99dd06010a5028679cf92b7f473271
arud95 <antonrud95@yandex.ru>
2021-01-11 01:17:42 +0200

    feature: add video section

--- components/index.js
+++ components/index.js
@@ -2,5 +2,6 @@ import Hero from './hero';
 import Feature from './features';
 import Header from './header';
 import Advantage from './advantages';
+import Video from './video';
 
-export { Header, Hero, Feature, Advantage };
+export { Header, Hero, Feature, Advantage, Video };
