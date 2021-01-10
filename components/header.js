591abd395334c464e3376f13f432f2c42a90e3a2
Anton Rud <56043786+antonrud95@users.noreply.github.com>
2021-01-10 15:41:59 +0200

    Sl 3 (#3)
    feature: add advantages section

--- components/header.js
+++ components/header.js
@@ -26,46 +26,48 @@ const Header = ({ links, button }) => {
     },
     [isOnTop]
   )
-    const navStyles = 'container mx-auto px-4 fixed right-0 left-0 top-0 py-25 z-1000 transition duration-500 ease-in-out'
-    return ( 
+    const navStyles = 'fixed right-0 left-0 top-0 py-25 z-1000 transition duration-500 ease-in-out'
+    return (
       <header className={isOnTop ? navStyles : classnames(navStyles, 'bg-white')}>
-        <div className='flex items-center justify-between '>
-          <Fade top>
-            <Logo />
-          </Fade>
-          <MobileMenu items={links} buttons={button} isShown={isMenuShown} toggle={toggleMenu}/>
-          <FontAwesomeIcon icon={faBars} onClick={openClickHandler} className={'block lg:hidden'}/>
-          <Fade top delay={300}>
-            <nav className='hidden lg:flex items-center'>
-                {links.map((link, index) => {
-                  return(
-                    <Link href="/" key={Math.random()}>
-                            <a className='transition duration-500 ease-in-out text-16-33 text-main hover:text-purple mr-xl'>
-                                {link}
-                            </a>
-                        </Link>
+        <div className="container mx-auto px-4">
+          <div className='flex items-center justify-between '>
+            <Fade top>
+              <Logo />
+            </Fade>
+            <MobileMenu items={links} buttons={button} isShown={isMenuShown} toggle={toggleMenu}/>
+            <FontAwesomeIcon icon={faBars} onClick={openClickHandler} className={'block lg:hidden'}/>
+            <Fade top delay={300}>
+              <nav className='hidden lg:flex items-center'>
+                  {links.map((link, index) => {
+                    return(
+                      <Link href="/" key={Math.random()}>
+                              <a className='transition duration-500 ease-in-out text-16-33 text-main hover:text-purple mr-xl'>
+                                  {link}
+                              </a>
+                          </Link>
+                      )
+                  })}
+              </nav>
+            </Fade>
+            <Fade top delay={400}>
+            <div className='hidden lg:flex'>
+                {button.map((btn) => {
+                    return(
+                        <Button 
+                        type={btn.type}
+                        additionalClassName={'text-16px text-purple py-13 px-25 tracking-button capitalize border-2 border-purple'}
+                        key={Math.random()}
+                        link={btn.link}
+                        >
+                          {btn.name}
+                        </Button>
                     )
                 })}
-            </nav>
-          </Fade>
-          <Fade top delay={400}>
-          <div className='hidden lg:flex'>
-              {button.map((btn) => {
-                  return(
-                      <Button 
-                      type={btn.type}
-                      additionalClassName={'text-16px text-purple py-13 px-25 tracking-button capitalize border-2 border-purple'}
-                      key={Math.random()}
-                      link={btn.link}
-                      >
-                        {btn.name}
-                      </Button>
-                  )
-              })}
+            </div>
+            </Fade>
           </div>
-          </Fade>
         </div>
-      </header>
+      </header> 
     );
   }
   
